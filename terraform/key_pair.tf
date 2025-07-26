d962993c139d9bb0a6d3f833bcd7e2084a85448d
Misha6Sharma <misha6shharma@gmail.com>
2025-07-26 23:43:37 +0530

    using git action for the first time

--- terraform/key_pair.tf
+++ terraform/key_pair.tf
@@ -1,4 +1,5 @@
 resource "aws_key_pair" "generated_key" {
   key_name   = "key_pair"
-  public_key = file("C:\\Users\\misha\\.ssh\\id_ed25519.pub")
+  public_key =  file("${path.module}/id_ed25519.pub")
+
 }
