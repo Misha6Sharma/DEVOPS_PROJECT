179335fde923e2fe5248d761222e6d2448d69537
Misha6Sharma <misha6shharma@gmail.com>
2025-07-26 23:21:11 +0530

    using git action for the first time

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "my_ec2" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.generated_key.key_name
+
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   subnet_id                   = var.subnet_id 
   associate_public_ip_address = true          
